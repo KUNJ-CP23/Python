59fab02bbba5b24dfc8b0904f4387eb99b76cae6
Kunj Ramoliya <kunjramoliya367@gmail.com>
2024-11-15 18:40:52 +0530

    Update Slicing.py

--- Basic Learning/Slicing.py
+++ Basic Learning/Slicing.py
@@ -1,10 +1,21 @@
 str = 'Kunj Ramoliya'
+
 #slicing
+
 print(str[0:3])
+
 print(str[6:len(str)])
+
 print(str[5:])
+
 print(str[:4])
 
+
 #negative index(backward counting)
+
 print(str[-8:-1])
+
 print(str[-8:])
+
+print(str[::2]) #skips one from both word
+
