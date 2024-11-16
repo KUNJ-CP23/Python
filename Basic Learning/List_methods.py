8468de6b6c51b7665573556f76a3d9e838887f29
Kunj Ramoliya <kunjramoliya367@gmail.com>
2024-11-16 17:45:45 +0530

    Lists added

---
+++ Basic Learning/List_methods.py
@@ -0,0 +1,27 @@
+#Lists methods
+list = [2,6,37,8,4,9,8,7]
+
+#append , adding 1 element at end
+list.append(23)
+print(list)
+
+#sort, in asc order
+list.sort()
+list.sort(reverse = True)
+print(list)
+
+#reverse
+list.reverse()
+print(list)
+
+#insert
+list.insert(3,98)
+print(list)
+
+#remove
+list.remove(2)
+print(list)
+
+#pop
+list.pop(5)
+print(list)
